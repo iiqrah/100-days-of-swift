560554a8a0c8848d2c41634fb07f11b23afddac3
iiqrah <nadeem.iqrah@stu.mmu.ac.uk>
2020-02-03 19:53:52 +0000

    Add Day 1 playground code

---
+++ Day1.playground/Contents.swift
@@ -0,0 +1,38 @@
+//: Playground - noun: a place where people can play
+
+var str: String = "Hello, playground!"
+str = "Goodbye!"
+
+var number = 10_000
+
+var multiLine = """
+This is weird
+multi line
+syntax.
+"""
+
+var double = 3.141576893
+
+var boolean: Bool = true
+
+
+// String Interpolation
+
+var age: Int = 21
+
+var ageMessage: String = "\(age) old"
+
+var message = """
+What is your age?
+I am \(ageMessage).
+"""
+
+//Constants
+
+let pi: Double = 3.14
+
+let QuoteOfTheDay = """
+The secret to getting ahead is getting started
+- Mark Twain
+"""
+
