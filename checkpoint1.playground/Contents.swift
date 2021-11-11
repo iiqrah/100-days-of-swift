179ff7893949d8a7bdc7d7edf46e07a1280a19aa
Iqrah Nadeem <nadeem.iqrah@stu.mmu.ac.uk>
2021-11-11 14:55:52 +0300

    Add checkpoint1

---
+++ checkpoint1.playground/Contents.swift
@@ -0,0 +1,7 @@
+//Option+Shift+8 to get the degrees symbol: °
+
+let cTemp = 89.0
+let fTemp = ((cTemp*9)/5)+32
+print ("Temperature in Celcius: \(cTemp)°C\nTemperature in Fahrenheit: \(fTemp)°F ")
+
+
