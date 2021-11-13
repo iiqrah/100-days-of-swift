4b59bbe4c93db11555f4f7027c7f0777f4dbd0ae
Iqrah Nadeem <nadeem.iqrah@stu.mmu.ac.uk>
2021-11-13 12:47:13 +0300

    checkpoint2 completed

---
+++ checkpoint2.playground/Contents.swift
@@ -0,0 +1,10 @@
+let albums = ["Red", "Fearless", "Red"]
+
+print (albums)
+print ("Number of items in the array: \(albums.count)")
+
+let uniqueAlbums = Set(albums)
+
+print (uniqueAlbums)
+print ("Number of unique items in the array: \(uniqueAlbums.count)")
+
