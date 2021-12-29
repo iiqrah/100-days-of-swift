901817f05e42286d3dc86e58fe1b209b6708d351
Iqrah Nadeem <nadeem.iqrah@stu.mmu.ac.uk>
2021-12-30 04:03:34 +0530

    Create Animations-1 Xcode project

---
+++ Animation/Animation-1/Animation-1/Animation_1App.swift
@@ -0,0 +1,17 @@
+//
+//  Animation_1App.swift
+//  Animation-1
+//
+//  Created by iiqrah on 30/12/2021.
+//
+
+import SwiftUI
+
+@main
+struct Animation_1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Animation/Animation-1/Animation-1/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Animation-1
+//
+//  Created by iiqrah on 30/12/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
