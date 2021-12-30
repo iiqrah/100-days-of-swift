b5d408ec8bf90f450739f9d3851039ae8d090a01
Iqrah Nadeem <nadeem.iqrah@stu.mmu.ac.uk>
2021-12-30 05:37:00 +0530

    Create Animation-2 XCode project

---
+++ Animation/Animation-2/Animation-2/Animation_2App.swift
@@ -0,0 +1,17 @@
+//
+//  Animation_2App.swift
+//  Animation-2
+//
+//  Created by iiqrah on 30/12/2021.
+//
+
+import SwiftUI
+
+@main
+struct Animation_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
