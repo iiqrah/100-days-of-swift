7d4921d7ca10ef59aa6826c3d6813b8093e3bb35
Iqrah Nadeem <nadeem.iqrah@stu.mmu.ac.uk>
2022-01-02 13:54:12 +0530

    Add explicit animation to snapback

--- Animation/Animation-2/Animation-2/ContentView.swift
+++ Animation/Animation-2/Animation-2/ContentView.swift
@@ -18,9 +18,13 @@ struct ContentView: View {
                     .gesture(
                         DragGesture()
                             .onChanged { dragAmount = $0.translation }
-                            .onEnded { _ in dragAmount = .zero }
+                            .onEnded { _ in
+                                withAnimation{
+                                    dragAmount = .zero
+                                }
+                            }
                     )
-                    .animation(.spring(), value: dragAmount)
+                    //.animation(.spring(), value: dragAmount)
         
     }
 }
