01b6783eec3eb2da5b63e85a2436e4b34894e206
Iqrah Nadeem <nadeem.iqrah@stu.mmu.ac.uk>
2022-01-02 15:16:30 +0530

    Create Animation-3 XCode project

---
+++ Animation/Animation-3/Animation-3/Animation_3App.swift
@@ -0,0 +1,17 @@
+//
+//  Animation_3App.swift
+//  Animation-3
+//
+//  Created by iiqrah on 02/01/2022.
+//
+
+import SwiftUI
+
+@main
+struct Animation_3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
