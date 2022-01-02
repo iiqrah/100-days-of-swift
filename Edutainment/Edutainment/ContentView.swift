1d47b5d107ae7ca5b983aef97050d8c65190a814
Iqrah Nadeem <nadeem.iqrah@stu.mmu.ac.uk>
2022-01-02 19:10:38 +0530

    Create Edutainment Xcode Project

---
+++ Edutainment/Edutainment/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  Edutainment
+//
+//  Created by iiqrah on 02/01/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    var body: some View {
+        
+                Text("Hello, World!")
+        
+
+        }
+    
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Edutainment/Edutainment/EdutainmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  EdutainmentApp.swift
+//  Edutainment
+//
+//  Created by iiqrah on 02/01/2022.
+//
+
+import SwiftUI
+
+@main
+struct EdutainmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
