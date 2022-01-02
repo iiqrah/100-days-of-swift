08d5eb74063a9c845e6a39a267a8d71e214c63da
Iqrah Nadeem <nadeem.iqrah@stu.mmu.ac.uk>
2022-01-02 14:03:13 +0530

    Add snake animation text

--- Animation/Animation-2/Animation-2/ContentView.swift
+++ Animation/Animation-2/Animation-2/ContentView.swift
@@ -8,23 +8,53 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var dragAmount = CGSize.zero
+    let letters = Array("Hello SwiftUI")
+    @State private var enabled = false
+    @State private var cardDragAmount = CGSize.zero
+    @State private var textDragAmount = CGSize.zero
     
     var body: some View {
-        LinearGradient(gradient: Gradient(colors: [.yellow, .red]), startPoint: .topLeading, endPoint: .bottomTrailing)
-                    .frame(width: 300, height: 200)
-                    .clipShape(RoundedRectangle(cornerRadius: 10))
-                    .offset(dragAmount)
-                    .gesture(
-                        DragGesture()
-                            .onChanged { dragAmount = $0.translation }
-                            .onEnded { _ in
-                                withAnimation{
-                                    dragAmount = .zero
+        
+        VStack{
+            
+            LinearGradient(gradient: Gradient(colors: [.yellow, .red]), startPoint: .topLeading, endPoint: .bottomTrailing)
+                        .frame(width: 300, height: 200)
+                        .clipShape(RoundedRectangle(cornerRadius: 10))
+                        .offset(cardDragAmount)
+                        .gesture(
+                            DragGesture()
+                                .onChanged { cardDragAmount = $0.translation }
+                                .onEnded { _ in
+                                    withAnimation{
+                                        cardDragAmount = .zero
+                                    }
                                 }
-                            }
-                    )
-                    //.animation(.spring(), value: dragAmount)
+                        )
+                        //.animation(.spring(), value: dragAmount)
+        Spacer()
+        
+        HStack(spacing: 0) {
+                    ForEach(0..<letters.count) { num in
+                        Text(String(letters[num]))
+                            .padding(5)
+                            .font(.title)
+                            .background(enabled ? .blue : .red)
+                            .offset(textDragAmount)
+                            .animation(.default.delay(Double(num) / 20), value: textDragAmount)
+                    }
+                }.gesture(
+                    DragGesture()
+                        .onChanged { textDragAmount = $0.translation }
+                        .onEnded { _ in
+                            textDragAmount = .zero
+                            enabled.toggle()
+                        }
+                )
+            
+        Spacer()
+            
+        }
+        
         
     }
 }
