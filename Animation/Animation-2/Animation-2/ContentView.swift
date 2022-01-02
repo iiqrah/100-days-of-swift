2084c5be7a97ed372e215df09c13ee7a95e704ac
Iqrah Nadeem <nadeem.iqrah@stu.mmu.ac.uk>
2022-01-02 13:52:40 +0530

    Add spring animated drag card

--- Animation/Animation-2/Animation-2/ContentView.swift
+++ Animation/Animation-2/Animation-2/ContentView.swift
@@ -8,10 +8,20 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var dragAmount = CGSize.zero
+    
     var body: some View {
         LinearGradient(gradient: Gradient(colors: [.yellow, .red]), startPoint: .topLeading, endPoint: .bottomTrailing)
                     .frame(width: 300, height: 200)
                     .clipShape(RoundedRectangle(cornerRadius: 10))
+                    .offset(dragAmount)
+                    .gesture(
+                        DragGesture()
+                            .onChanged { dragAmount = $0.translation }
+                            .onEnded { _ in dragAmount = .zero }
+                    )
+                    .animation(.spring(), value: dragAmount)
+        
     }
 }
 
