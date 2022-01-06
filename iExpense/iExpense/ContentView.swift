e4aa1c8f729af2f1ab425654f2d3ddb0964eb7e4
Iqrah Nadeem <nadeem.iqrah@stu.mmu.ac.uk>
2022-01-06 22:02:29 +0530

    Add toolbar kit to test add item

--- iExpense/iExpense/ContentView.swift
+++ iExpense/iExpense/ContentView.swift
@@ -8,9 +8,33 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @StateObject var expenses = Expenses()
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView{
+            
+            List{
+                
+                ForEach(expenses.items, id: \.name){ item in
+                    
+                    Text(item.name)
+                    
+                }
+            }
+            .navigationTitle("iExpense")
+            .toolbar{
+                Button{
+                    let expense = ExpenseItem(name: "Test", category: "Personal", cost: 67.98)
+                    expenses.items.append(expense)
+                }
+            label: {
+                Image(systemName: "plus")
+            }
+            }
+            
+            
+        }
     }
 }
 
