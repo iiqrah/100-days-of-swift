256788d6d6f299df3908374ca526a73a6b83f33c
Iqrah Nadeem <nadeem.iqrah@stu.mmu.ac.uk>
2022-01-07 01:40:11 +0530

    Add Codable for JSON data

--- iExpense/iExpense/ExpenseItem.swift
+++ iExpense/iExpense/ExpenseItem.swift
@@ -8,8 +8,8 @@
 import Foundation
 
 //Add Identifiable protocol to remove it from ContentView file as SwiftUI now knows how to uniquiely identify each entity
-struct ExpenseItem  : Identifiable{
-    let id = UUID() //Generates a unique hex-decimal id
+struct ExpenseItem  : Identifiable, Codable{
+    var id = UUID() //Generates a unique hex-decimal id
     let name: String
     let category: String
     let cost: Double
