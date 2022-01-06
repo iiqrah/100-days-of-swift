ebe5ea3f527c7fa14fc3c0a13ccd5a3342a581a1
Iqrah Nadeem <nadeem.iqrah@stu.mmu.ac.uk>
2022-01-06 22:01:41 +0530

    Add ExpenseItem struct

---
+++ iExpense/iExpense/ExpenseItem.swift
@@ -0,0 +1,15 @@
+//
+//  ExpenseItem.swift
+//  iExpense
+//
+//  Created by iiqrah on 06/01/2022.
+//
+
+import Foundation
+
+struct ExpenseItem {
+    
+    let name: String
+    let category: String
+    let cost: Double
+}
