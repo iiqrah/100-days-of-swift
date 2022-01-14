44edff2aedd4216cc303f5bc97a31cba4477d9e9
Iqrah Nadeem <nadeem.iqrah@stu.mmu.ac.uk>
2022-01-14 23:48:42 +0530

    Add ForEach List items and Toolbar Add Button

--- iExpense/iExpense/ContentView.swift
+++ iExpense/iExpense/ContentView.swift
@@ -8,9 +8,32 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @StateObject var expenses = Expenses()
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView{
+            
+            List{
+                
+                ForEach(expenses.items, id:\.name){ item in
+                    Text(item.name)
+                }
+            }
+            .toolbar{
+                Button{
+                    
+                    let expense = ExpenseItem(name: "Test", type: "Personal", amount: 0.0)
+                    
+                    expenses.items.append(expense)
+                    
+                }label:{
+                    Image(systemName: "plus")
+                }
+            }
+            .navigationTitle("iExpense")
+            
+        }
     }
 }
 
