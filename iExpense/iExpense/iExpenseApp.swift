60a2ff89f33a98d20e6a536ead787d8a41e9b5ed
Iqrah Nadeem <nadeem.iqrah@stu.mmu.ac.uk>
2022-01-14 21:05:20 +0530

    Re-create iExpense XCode project

---
+++ iExpense/iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by iiqrah on 14/01/2022.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
