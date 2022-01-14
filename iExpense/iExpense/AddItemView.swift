0020c27546a2dc4e35f05029201573f8c4c7b259
Iqrah Nadeem <nadeem.iqrah@stu.mmu.ac.uk>
2022-01-15 00:11:04 +0530

    Create AddItemView

---
+++ iExpense/iExpense/AddItemView.swift
@@ -0,0 +1,46 @@
+//
+//  AddItemView.swift
+//  iExpense
+//
+//  Created by iiqrah on 15/01/2022.
+//
+
+import SwiftUI
+
+struct AddItemView: View {
+    
+    @State private var name = ""
+    
+    @State private var type = "Personal"
+    let types = ["Personal", "Business"]
+    
+    @State private var amount = 0.0
+    
+    
+    var body: some View {
+        NavigationView{
+            
+            Form{
+                TextField("Item Name: ", text: $name)
+                
+                Picker("Type: ", selection: $type){
+                    ForEach(types, id:\.self){
+                        Text($0)
+                    }
+                }
+                
+                TextField("Amount: ", value: $amount, format: .currency(code: "USD"))
+                    .keyboardType(.decimalPad)
+                
+                
+            }.navigationTitle("Add Item")
+            
+        }
+    }
+}
+
+struct AddItemView_Previews: PreviewProvider {
+    static var previews: some View {
+        AddItemView()
+    }
+}
