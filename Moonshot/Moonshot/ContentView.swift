78cc8bc18d8b0c1aeede35768269f5d8e53f5c6c
Iqrah Nadeem <nadeem.iqrah@stu.mmu.ac.uk>
2022-01-17 02:30:03 +0530

    Test missions decoded data

--- Moonshot/Moonshot/ContentView.swift
+++ Moonshot/Moonshot/ContentView.swift
@@ -8,9 +8,17 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    //astronauts.json is a dictionary
+    let astronauts: [String: Astronaut] = Bundle.main.decode("astronauts.json")
+    
+    //missions.json is an array
+    let missions: [Mission] = Bundle.main.decode("missions.json")
+
+
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        Text("Count: \(missions.count)")
     }
 }
 
