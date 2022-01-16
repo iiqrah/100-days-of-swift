ba4a4855b5bcb1785b713e1c1e21ed446dcee497
Iqrah Nadeem <nadeem.iqrah@stu.mmu.ac.uk>
2022-01-16 19:26:07 +0530

    Create Astronaut struct

---
+++ Moonshot/Moonshot/Astronaut.swift
@@ -0,0 +1,16 @@
+//
+//  Astronaut.swift
+//  Moonshot
+//
+//  Created by iiqrah on 16/01/2022.
+//
+
+import Foundation
+
+struct Astronaut: Identifiable, Codable {
+    
+    let id: String
+    let name: String
+    let description: String
+    
+}
