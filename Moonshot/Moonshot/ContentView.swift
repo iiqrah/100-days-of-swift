9e45d5f3af8e38705ebcdb9bd01dc136472a1e14
Iqrah Nadeem <nadeem.iqrah@stu.mmu.ac.uk>
2022-01-18 04:37:58 +0530

    Attach to GridLayout view

--- Moonshot/Moonshot/ContentView.swift
+++ Moonshot/Moonshot/ContentView.swift
@@ -29,66 +29,12 @@ struct ContentView: View {
                 
                 
                 if isShowingGrid {
-                        //GridLayout(astronauts: astronauts, missions: missions)
-                    Text("Showing Grid")
+                    GridLayout()
                     } else {
                         //ListLayout(astronauts: astronauts, missions: missions)
                         Text("Showing List")
                     }
                 
-                LazyVGrid(columns: columns){
-                    
-                    ForEach(missions) { mission in
-                        
-                        NavigationLink{
-                            MissionDetailView(mission: mission, astronauts: astronauts)
-                        }
-                    label:{
-                        
-                        VStack{
-                            
-                            Image(mission.imageName)
-                                .resizable()
-                                .scaledToFit()
-                                .frame(width: 100, height: 100)
-                                .padding()
-                            
-                            VStack{
-                                
-                                
-                                Text(mission.displayName)
-                                    .font(.headline)
-                                    .foregroundColor(.white)
-
-                                Text(mission.formattedLaunchDate)
-                                    .font(.caption)
-                                    .foregroundColor(.white.opacity(0.5))
-
-                                
-                                
-                            }
-                            .padding(.vertical)
-                            .frame(maxWidth: .infinity)
-                            .background(.lightBackground)
-                            
-                        }
-                        
-                        .clipShape(RoundedRectangle(cornerRadius: 10))
-                        .overlay(
-                            RoundedRectangle(cornerRadius: 10)
-                                .stroke(.lightBackground)
-                        )
-                        
-                        
-                    }
-                        
-                    }
-                    
-                    
-                    
-                }
-                
-                .padding([.horizontal, .bottom])
             }
             
             .navigationTitle("Moonshot")
