08e26a6be57dba04e4566cc5963a7998ee3bb40e
Iqrah Nadeem <nadeem.iqrah@stu.mmu.ac.uk>
2022-01-19 22:36:38 +0530

    Create Drawing XCode project

---
+++ Drawing/Drawing/DrawingApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrawingApp.swift
+//  Drawing
+//
+//  Created by iiqrah on 19/01/2022.
+//
+
+import SwiftUI
+
+@main
+struct DrawingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
