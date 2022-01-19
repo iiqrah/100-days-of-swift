84d0a3af12423d35e09f055794b640255da197f3
Iqrah Nadeem <nadeem.iqrah@stu.mmu.ac.uk>
2022-01-19 22:51:04 +0530

    Draw lines to make house

--- Drawing/Drawing/ContentView.swift
+++ Drawing/Drawing/ContentView.swift
@@ -9,8 +9,29 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        
+        //triangle drawn anti-clockwise
+        //CG stands for Core Graphics
+        Path { path in
+            path.move(to: CGPoint(x: 100, y: 300))
+            path.addLine(to: CGPoint(x: 200, y: 100))
+            path.addLine(to: CGPoint(x: 300, y: 300))
+            path.addLine(to: CGPoint(x: 100, y: 300))
+            
+            path.addLine(to: CGPoint(x: 100, y: 500))
+            path.addLine(to: CGPoint(x: 300, y: 500))
+            path.addLine(to: CGPoint(x: 300, y: 300))
+
+            //make all the lines joint
+            //path.closeSubpath()
+
+        }
+        //to fill the shape
+        //.fill(.yellow)
+        //to stroke the shape
+        //.stroke(.blue, lineWidth: 10)
+        .stroke(.yellow, style: StrokeStyle(lineWidth: 10, lineCap: .round, lineJoin: .round))
+
     }
 }
 
