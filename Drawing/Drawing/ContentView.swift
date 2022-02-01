6edcd74697e54d7de299865d4e76b30042a8cbf4
Iqrah Nadeem <nadeem.iqrah@stu.mmu.ac.uk>
2022-02-01 16:33:00 +0530

    Add Triangle Shape

--- Drawing/Drawing/ContentView.swift
+++ Drawing/Drawing/ContentView.swift
@@ -7,30 +7,78 @@
 
 import SwiftUI
 
+struct Triangle: Shape{
+    func path(in rect: CGRect) -> Path {
+        var path = Path()
+        
+        path.move(to: CGPoint(x: rect.midX, y: rect.minY))
+        path.addLine(to: CGPoint(x: rect.minX, y: rect.maxY))
+        path.addLine(to: CGPoint(x: rect.maxX, y: rect.maxY))
+        path.addLine(to: CGPoint(x: rect.midX, y: rect.minY))
+        
+        
+        return path
+        
+    }
+}
+
 struct ContentView: View {
     var body: some View {
         
-        //triangle drawn anti-clockwise
-        //CG stands for Core Graphics
-        Path { path in
-            path.move(to: CGPoint(x: 100, y: 300))
-            path.addLine(to: CGPoint(x: 200, y: 100))
-            path.addLine(to: CGPoint(x: 300, y: 300))
-            path.addLine(to: CGPoint(x: 100, y: 300))
+        VStack{
             
-            path.addLine(to: CGPoint(x: 100, y: 500))
-            path.addLine(to: CGPoint(x: 300, y: 500))
-            path.addLine(to: CGPoint(x: 300, y: 300))
+            Section{
+                
+                //Path: Fixed and non-reusable
+                
+                //triangle drawn anti-clockwise
+                //CG stands for Core Graphics
+                Path { path in
+                    path.move(to: CGPoint(x: 100, y: 300))
+                    path.addLine(to: CGPoint(x: 200, y: 100))
+                    path.addLine(to: CGPoint(x: 300, y: 300))
+                    path.addLine(to: CGPoint(x: 100, y: 300))
+                    
+                    path.addLine(to: CGPoint(x: 100, y: 500))
+                    path.addLine(to: CGPoint(x: 300, y: 500))
+                    path.addLine(to: CGPoint(x: 300, y: 300))
 
-            //make all the lines joint
-            //path.closeSubpath()
+                    //make all the lines joint
+                    //path.closeSubpath()
+                    
+                }
+                //to fill the shape
+                //.fill(.yellow)
+                //to stroke the shape
+                //.stroke(.blue, lineWidth: 10)
+                .stroke(.yellow, style: StrokeStyle(lineWidth: 10, lineCap: .round, lineJoin: .round))
+                
+                
+            }
+            
+            
+            
+            Section{
+                
+                //Shapes: reusability
+                
+                Triangle()
+                //.fill(.red)
+                .stroke(.red, style: StrokeStyle(lineWidth: 10, lineCap: .round, lineJoin: .round))
 
+                .frame(width: 300, height: 300)
+            }
+            
+            
+            
+            
+            
         }
-        //to fill the shape
-        //.fill(.yellow)
-        //to stroke the shape
-        //.stroke(.blue, lineWidth: 10)
-        .stroke(.yellow, style: StrokeStyle(lineWidth: 10, lineCap: .round, lineJoin: .round))
+        
+       
+        
+        
+        
 
     }
 }
