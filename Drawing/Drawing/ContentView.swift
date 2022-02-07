63c284aae5ec0e1948ba8a9e46ef6acd72d4af2d
Iqrah Nadeem <nadeem.iqrah@stu.mmu.ac.uk>
2022-02-07 10:19:38 +0530

    Add animation to checkerboard with AnimatablePair

--- Drawing/Drawing/ContentView.swift
+++ Drawing/Drawing/ContentView.swift
@@ -7,6 +7,48 @@
 
 import SwiftUI
 
+struct Checkerboard: Shape {
+    var rows: Int
+    var columns: Int
+    
+    var animatableData: AnimatablePair<Double, Double> {
+        get {
+           AnimatablePair(Double(rows), Double(columns))
+        }
+
+        set {
+            rows = Int(newValue.first)
+            columns = Int(newValue.second)
+        }
+    }
+
+    func path(in rect: CGRect) -> Path {
+        var path = Path()
+
+        // figure out how big each row/column needs to be
+        let rowSize = rect.height / Double(rows)
+        let columnSize = rect.width / Double(columns)
+
+        // loop over all rows and columns, making alternating squares colored
+        for row in 0..<rows {
+            for column in 0..<columns {
+                if (row + column).isMultiple(of: 2) {
+                    // this square should be colored; add a rectangle here
+                    let startX = columnSize * Double(column)
+                    let startY = rowSize * Double(row)
+
+                    let rect = CGRect(x: startX, y: startY, width: columnSize, height: rowSize)
+                    path.addRect(rect)
+                }
+            }
+        }
+
+        return path
+    }
+}
+
+
+
 struct Trapezoid: Shape {
     var insetAmount: Double
     
@@ -158,6 +200,10 @@ struct ContentView: View {
 
     
     @State private var insetAmount = 50.0
+    
+    
+    @State private var rows = 4
+    @State private var columns = 4
 
 
     
@@ -398,14 +444,25 @@ struct ContentView: View {
 //
 //            }
             
+//
+//            Section{
+//
+//                Trapezoid(insetAmount: insetAmount)
+//                            .frame(width: 200, height: 100)
+//                            .onTapGesture {
+//                                withAnimation {
+//                                    insetAmount = Double.random(in: 10...90)
+//                                }
+//                            }
+//            }
             
             Section{
                 
-                Trapezoid(insetAmount: insetAmount)
-                            .frame(width: 200, height: 100)
+                Checkerboard(rows: rows, columns: columns)
                             .onTapGesture {
-                                withAnimation {
-                                    insetAmount = Double.random(in: 10...90)
+                                withAnimation(.linear(duration: 3)) {
+                                    rows = 8
+                                    columns = 16
                                 }
                             }
             }
