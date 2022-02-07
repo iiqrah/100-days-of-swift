2a17536c051ecda2045c7f8c035b2757abc49997
Iqrah Nadeem <nadeem.iqrah@stu.mmu.ac.uk>
2022-02-07 10:30:41 +0530

    Create customizable Spirograph

--- Drawing/Drawing/ContentView.swift
+++ Drawing/Drawing/ContentView.swift
@@ -7,6 +7,54 @@
 
 import SwiftUI
 
+struct Spirograph: Shape {
+    let innerRadius: Int
+    let outerRadius: Int
+    let distance: Int
+    let amount: Double
+    
+    func gcd(_ a: Int, _ b: Int) -> Int {
+        var a = a
+        var b = b
+
+        while b != 0 {
+            let temp = b
+            b = a % b
+            a = temp
+        }
+
+        return a
+    }
+    
+    
+    func path(in rect: CGRect) -> Path {
+        let divisor = gcd(innerRadius, outerRadius)
+        let outerRadius = Double(self.outerRadius)
+        let innerRadius = Double(self.innerRadius)
+        let distance = Double(self.distance)
+        let difference = innerRadius - outerRadius
+        let endPoint = ceil(2 * Double.pi * outerRadius / Double(divisor)) * amount
+
+        var path = Path()
+
+        for theta in stride(from: 0, through: endPoint, by: 0.01) {
+            var x = difference * cos(theta) + distance * cos(difference / outerRadius * theta)
+            var y = difference * sin(theta) - distance * sin(difference / outerRadius * theta)
+
+            x += rect.width / 2
+            y += rect.height / 2
+
+            if theta == 0 {
+                path.move(to: CGPoint(x: x, y: y))
+            } else {
+                path.addLine(to: CGPoint(x: x, y: y))
+            }
+        }
+
+        return path
+    }
+}
+
 struct Checkerboard: Shape {
     var rows: Int
     var columns: Int
@@ -196,7 +244,7 @@ struct ContentView: View {
     @State private var colorCycle = 0.0
     
     
-    @State private var amount = 0.0
+    @State private var circleAmount = 0.0
 
     
     @State private var insetAmount = 50.0
@@ -204,6 +252,13 @@ struct ContentView: View {
     
     @State private var rows = 4
     @State private var columns = 4
+    
+    
+    @State private var innerRadius = 125.0
+    @State private var outerRadius = 75.0
+    @State private var distance = 25.0
+    @State private var amount = 1.0
+    @State private var hue = 0.6
 
 
     
@@ -399,24 +454,24 @@ struct ContentView: View {
 //                            ZStack {
 //                                Circle()
 //                                    .fill(.red)
-//                                    .frame(width: 200 * amount)
+//                                    .frame(width: 200 * circleAmount)
 //                                    .offset(x: -50, y: -80)
 //                                    .blendMode(.screen)
 //
 //                                Circle()
 //                                    .fill(.green)
-//                                    .frame(width: 200 * amount)
+//                                    .frame(width: 200 * circleAmount)
 //                                    .offset(x: 50, y: -80)
 //                                    .blendMode(.screen)
 //
 //                                Circle()
 //                                    .fill(.blue)
-//                                    .frame(width: 200 * amount)
+//                                    .frame(width: 200 * circleAmount)
 //                                    .blendMode(.screen)
 //                            }
 //                            .frame(width: 300, height: 300)
 //
-//                            Slider(value: $amount)
+//                            Slider(value: $circleAmount)
 //                                .padding()
 //                        }
 //                        .frame(maxWidth: .infinity, maxHeight: .infinity)
@@ -456,17 +511,52 @@ struct ContentView: View {
 //                            }
 //            }
             
-            Section{
-                
-                Checkerboard(rows: rows, columns: columns)
-                            .onTapGesture {
-                                withAnimation(.linear(duration: 3)) {
-                                    rows = 8
-                                    columns = 16
-                                }
-                            }
-            }
+//            Section{
+//
+//                Checkerboard(rows: rows, columns: columns)
+//                            .onTapGesture {
+//                                withAnimation(.linear(duration: 3)) {
+//                                    rows = 8
+//                                    columns = 16
+//                                }
+//                            }
+//            }
+//
             
+            
+            Section{
+                VStack(spacing: 0) {
+                            Spacer()
+
+                            Spirograph(innerRadius: Int(innerRadius), outerRadius: Int(outerRadius), distance: Int(distance), amount: amount)
+                                .stroke(Color(hue: hue, saturation: 1, brightness: 1), lineWidth: 1)
+                                .frame(width: 300, height: 300)
+
+                            Spacer()
+
+                            Group {
+                                Text("Inner radius: \(Int(innerRadius))")
+                                Slider(value: $innerRadius, in: 10...150, step: 1)
+                                    .padding([.horizontal, .bottom])
+
+                                Text("Outer radius: \(Int(outerRadius))")
+                                Slider(value: $outerRadius, in: 10...150, step: 1)
+                                    .padding([.horizontal, .bottom])
+
+                                Text("Distance: \(Int(distance))")
+                                Slider(value: $distance, in: 1...150, step: 1)
+                                    .padding([.horizontal, .bottom])
+
+                                Text("Amount: \(amount, format: .number.precision(.fractionLength(2)))")
+                                Slider(value: $amount)
+                                    .padding([.horizontal, .bottom])
+
+                                Text("Color")
+                                Slider(value: $hue)
+                                    .padding(.horizontal)
+                            }
+                        }
+            }
             
             
             
