f7daa9ae40723bde20ba6506aae1c5c8c042bbf4
Iqrah Nadeem <nadeem.iqrah@stu.mmu.ac.uk>
2022-02-07 09:56:36 +0530

    Draw ColorCyclingCircle

--- Drawing/Drawing/ContentView.swift
+++ Drawing/Drawing/ContentView.swift
@@ -7,6 +7,43 @@
 
 import SwiftUI
 
+struct ColorCyclingCircle: View {
+    var amount = 0.0
+    var steps = 100
+
+    var body: some View {
+        ZStack {
+            ForEach(0..<steps) { value in
+                Circle()
+                    .inset(by: Double(value))
+                    .strokeBorder(
+                        LinearGradient(
+                            gradient: Gradient(colors: [
+                                color(for: value, brightness: 1),
+                                color(for: value, brightness: 0.5)
+                            ]),
+                            startPoint: .top,
+                            endPoint: .bottom
+                        ),
+                        lineWidth: 2
+                    )
+            }
+        }
+        .drawingGroup()
+
+    }
+
+    func color(for value: Int, brightness: Double) -> Color {
+        var targetHue = Double(value) / Double(steps) + amount
+
+        if targetHue > 1 {
+            targetHue -= 1
+        }
+
+        return Color(hue: targetHue, saturation: 1, brightness: brightness)
+    }
+}
+
 struct Flower: Shape{
     
     var petalOffset: Double = -20
@@ -92,6 +129,9 @@ struct ContentView: View {
     
     @State private var petalOffset = -20.0
     @State private var petalWidth = 100.0
+    
+    @State private var colorCycle = 0.0
+
     
     var body: some View {
         
@@ -240,6 +280,17 @@ struct ContentView: View {
                         .frame(width: 150, height: 150)
                 }
             }
+            
+            
+            Section {
+                VStack {
+                            ColorCyclingCircle(amount: colorCycle)
+                                .frame(width: 300, height: 300)
+
+                            Slider(value: $colorCycle)
+                        }
+            }
+            
         }
         
         
