2cc6d0819bb58cf41dfa3f594bd5e7cb29166e07
Iqrah Nadeem <nadeem.iqrah@stu.mmu.ac.uk>
2022-02-07 10:09:24 +0530

    Add blur and colour to an image with slider

--- Drawing/Drawing/ContentView.swift
+++ Drawing/Drawing/ContentView.swift
@@ -322,37 +322,54 @@ struct ContentView: View {
 //            }
             
             
+//            Section{
+//
+//
+//                VStack {
+//                            ZStack {
+//                                Circle()
+//                                    .fill(.red)
+//                                    .frame(width: 200 * amount)
+//                                    .offset(x: -50, y: -80)
+//                                    .blendMode(.screen)
+//
+//                                Circle()
+//                                    .fill(.green)
+//                                    .frame(width: 200 * amount)
+//                                    .offset(x: 50, y: -80)
+//                                    .blendMode(.screen)
+//
+//                                Circle()
+//                                    .fill(.blue)
+//                                    .frame(width: 200 * amount)
+//                                    .blendMode(.screen)
+//                            }
+//                            .frame(width: 300, height: 300)
+//
+//                            Slider(value: $amount)
+//                                .padding()
+//                        }
+//                        .frame(maxWidth: .infinity, maxHeight: .infinity)
+//                        .background(.black)
+//
+//
+//
+//
+//            }
+            
+            
             Section{
                 
+                Image("singapore")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 200, height: 200)
+                    .saturation(amount)
+                    .blur(radius: (1 - amount) * 20)
                 
-                VStack {
-                            ZStack {
-                                Circle()
-                                    .fill(.red)
-                                    .frame(width: 200 * amount)
-                                    .offset(x: -50, y: -80)
-                                    .blendMode(.screen)
-
-                                Circle()
-                                    .fill(.green)
-                                    .frame(width: 200 * amount)
-                                    .offset(x: 50, y: -80)
-                                    .blendMode(.screen)
-
-                                Circle()
-                                    .fill(.blue)
-                                    .frame(width: 200 * amount)
-                                    .blendMode(.screen)
-                            }
-                            .frame(width: 300, height: 300)
-
-                            Slider(value: $amount)
+                
+                Slider(value: $amount)
                                 .padding()
-                        }
-                        .frame(maxWidth: .infinity, maxHeight: .infinity)
-                        .background(.black)
-                
-                
                 
                 
             }
