91656210305dd49048e36e19d05f03e3958c43bd
Iqrah Nadeem <nadeem.iqrah@stu.mmu.ac.uk>
2022-02-07 09:27:26 +0530

    Add Flower Shape

--- Drawing/Drawing/ContentView.swift
+++ Drawing/Drawing/ContentView.swift
@@ -7,6 +7,36 @@
 
 import SwiftUI
 
+struct Flower: Shape{
+    
+    var petalOffset: Double = -20
+    var petalWidth: Double = 100
+    
+    func path(in rect: CGRect) -> Path {
+        var path = Path()
+        
+        // from 0 - 360 degrees, draw 16 petals in total
+        //
+        for number in stride(from: 0, to: Double.pi * 2, by: Double.pi / 8) {
+            // rotate the petal by the current value of our loop
+            let rotation = CGAffineTransform(rotationAngle: number)
+            
+            // move the petal to be at the center of our view
+            let position = rotation.concatenating(CGAffineTransform(translationX: rect.width / 2, y: rect.height / 2))
+            
+            // create a path for this petal using our properties plus a fixed Y and height
+            let originalPetal = Path(ellipseIn: CGRect(x: petalOffset, y: 0, width: petalWidth, height: rect.width / 2))
+            
+            // apply our rotation/position transformation to the petal
+            let rotatedPetal = originalPetal.applying(position)
+            
+            // add it to our main path
+            path.addPath(rotatedPetal)
+        }
+        return path
+    }
+}
+
 struct Triangle: Shape{
     func path(in rect: CGRect) -> Path {
         var path = Path()
@@ -59,6 +89,10 @@ struct Arc:InsettableShape{
 }
 
 struct ContentView: View {
+    
+    @State private var petalOffset = -20.0
+    @State private var petalWidth = 100.0
+    
     var body: some View {
         
 //       //Day 43 - Project 9, part 1
@@ -169,9 +203,22 @@ struct ContentView: View {
 //        }
         
         
-        Text("Hello World!")
+        //       //Day 43 - Project 9, part 1
         
-       
+        VStack {
+            Flower(petalOffset: petalOffset, petalWidth: petalWidth)
+                .fill(.red, style: FillStyle(eoFill: true))
+
+            Text("Offset")
+            Slider(value: $petalOffset, in: -40...40)
+                .padding([.horizontal, .bottom])
+            
+            Text("Width")
+            Slider(value: $petalWidth, in: 0...100)
+                .padding(.horizontal)
+        }
+        
+        
         
         
         
