86b37fa37712c5938cd67c100e355409c19be95a
Iqrah Nadeem <nadeem.iqrah@stu.mmu.ac.uk>
2022-02-27 22:19:48 +0530

    Remove extra whitelines

--- CupcakeCorner/CupcakeCorner/AddAddressView.swift
+++ CupcakeCorner/CupcakeCorner/AddAddressView.swift
@@ -15,20 +15,14 @@ struct AddAddressView: View {
             Form{
 
             Section{
-
-
                     TextField("Name ", text: $order.name)
                     TextField("Street address ", text: $order.streetAddress)
                     TextField("City ", text: $order.city)
                     TextField("State ", text: $order.state)
                     TextField("Zip Code ", text: $order.zip)
-
-
             }
-
             Section{
                 NavigationLink{
-
                     CheckoutView(order: order)
                 }
             label: {
@@ -36,9 +30,7 @@ struct AddAddressView: View {
                 Text("Proceed to check-out")
             }
             }.disabled(order.hasValidAddress == false)
-
         }
-
             .navigationTitle("Delivery Details")
             .navigationBarTitleDisplayMode(.inline)
     }
