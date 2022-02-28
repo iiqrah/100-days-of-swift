08345fd0a1a12a6340b587270e47ca487f32d641
Iqrah Nadeem <nadeem.iqrah@stu.mmu.ac.uk>
2022-02-28 17:13:39 +0530

    Add async placeOrder()

--- CupcakeCorner/CupcakeCorner/CheckoutView.swift
+++ CupcakeCorner/CupcakeCorner/CheckoutView.swift
@@ -11,6 +11,9 @@ struct CheckoutView: View {
 
     @ObservedObject var order: Order
 
+    @State private var checkoutMessage = ""
+    @State private var isCheckoutMessageShowing = false
+
     var body: some View {
         ScrollView{
 
@@ -32,7 +35,11 @@ struct CheckoutView: View {
                 Text("Your total cost is \(order.cost, format: .currency(code: "USD"))")
                     .font(.title)
 
-                Button("Place Order", action: {})
+                Button("Place Order"){
+                    Task{
+                        await placeOrder()
+                    }
+                }
                     .padding()
 
 
@@ -41,6 +48,42 @@ struct CheckoutView: View {
 
         .navigationTitle("Check-out")
         .navigationBarTitleDisplayMode(.inline)
+        .alert("Order status", isPresented: $isCheckoutMessageShowing){
+
+            Button("OK"){}
+        }message:{
+
+            Text(checkoutMessage)
+        }
+    }
+
+    func placeOrder() async {
+
+        //1. Encoding data in jSON format
+        guard let encoded = try? JSONEncoder().encode(order) else{
+            print("Failed to encode order")
+            return
+        }
+        //2. Set up network call request to send over JSON encoded data
+        let url = URL(string: "https://reqres.in/api/cupcakes")!
+        var request = URLRequest(url:url)
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.httpMethod = "POST"
+
+        //3. Run the request and process the response
+        do{
+            let (data, _) = try await URLSession.shared.upload(for: request, from: encoded)
+
+            let decodedOrder = try JSONDecoder().decode(Order.self, from: data)
+            checkoutMessage = "Your order for \(decodedOrder.quantity) \(Order.types[decodedOrder.type].lowercased()) cupcakes is on its way!"
+            isCheckoutMessageShowing = true
+        }
+        catch {
+            print("Checkout failed.")
+            //Challenge 2
+            checkoutMessage = "Your order failed, please try again."
+            isCheckoutMessageShowing = true
+        }
     }
 }
 
